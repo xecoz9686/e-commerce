40230624c5ab445c1f30202f544572b6d1394a9e
xecoz9686 <shreenidhi9686@gmail.com>
2024-05-31 12:09:24 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div><center>
+      <h2>ABOUT PAGE 🛒</h2>
+      <h4>ALL YOUR NEEDS ARE HERE...OUR WEBSITE IS REPRESENTED IN TATVA INDIA,YOU CAN TRUST OUR AND SHOP NOW</h4>
+      </center>
+      <img src='https://pptmon.com/wp-content/uploads/2022/07/E-commerce-Free-Presentation-Template.png'></img>;
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+<div>
+      <img src='https://www.homesecurityheroes.com/wp-content/uploads/2023/05/image-17.png'></img>
+    </div>
+  )
+}
